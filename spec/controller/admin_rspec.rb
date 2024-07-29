177a9da1ae11030f15d888ee35fc1e4605b03947
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-07-29 16:11:01 +0530

    initial spec

--- spec/controller/admin_rspec.rb
+++ spec/controller/admin_rspec.rb
@@ -8,25 +8,25 @@ RSpec.describe AdminsController, type: :controller do
     end
  
     it "returns a success response" do
-        admin = Admin.create!(name: 'Test4', mobile: '59754754875', password_digest: 'password')
+        admin = Admin.create!(name: 'Test4', mobile: '5975475487', password_digest: 'password')
         get :show, params:{id: admin.id}
         expect(response).to be_successful
     end
 
     it "New Admin" do
-        admin = Admin.new(name: 'Test4', mobile: '59754754875', password_digest: 'password')
+        admin = Admin.new(name: 'Test4', mobile: '5975475487', password_digest: 'password')
         get :index
         expect(admin).to be_valid
     end
 
     it "create Admin" do
-        admin = Admin.create!(name: 'Test4', mobile: '59754754875', password_digest: 'password')
+        admin = Admin.create!(name: 'Test4', mobile: '5975475487', password_digest: 'password')
         get :index
         expect(response).to be_successful
     end
 
     it "delete Admin successfully" do
-        admin = Admin.create!(name: 'Test4', mobile: '59754754875', password_digest: 'password')
+        admin = Admin.create!(name: 'Test4', mobile: '5975475487', password_digest: 'password')
         delete :destroy, params:{id: admin.id}
         expect(response).to redirect_to(admins_url) 
     end
