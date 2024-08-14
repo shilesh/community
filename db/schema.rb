87484567eaf2f49c7015a5b3d755f57b632409fc
sourabh-sawant-v2 <sourabh.sawant@v2solutions.com>
2024-08-14 10:44:04 +0530

    users table is removed from DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_16_180024) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_14_051005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,13 +65,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_16_180024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["token"], name: "index_sessions_on_token", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
